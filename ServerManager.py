df7e4b94a47e5b9c654b9b494d109abc50fcdbb3
GamingOnInsulin <gamingoninsulin@gmail.com>
2024-02-17 19:58:24 +0100

    update for dumb ai

--- ServerManager.py
+++ ServerManager.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 import sys
 from multiprocessing import process
-
+from mcstatus import JavaServer
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
                             QMetaObject, QObject, QPoint, QRect,
                             QSize, QTime, QUrl, Qt, QThread, Signal, QThreadPool, QRunnable)
@@ -57,8 +57,11 @@ class OutputThread(QThread):
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
+            MainWindow.setWindowTitle("Minecraft Server Manager")
+            MainWindow.setWindowIcon(QIcon('grass_block.png'))
+            MainWindow.setStyleSheet("background-color: lightblue;")
         MainWindow.resize(1126, 852)
+
 
         self.actionOpen_EULA = QAction(MainWindow)
         self.actionOpen_EULA.setObjectName(u"actionOpen_EULA")
@@ -92,6 +95,15 @@ class Ui_MainWindow(object):
         self.actionOpen_config = QAction(MainWindow)
         self.actionOpen_config.setObjectName(u"actionOpen_config")
 
+        self.actionScheduleAdd = QAction(MainWindow)
+        self.actionScheduleAdd.setObjectName(u"actionScheduleAdd")
+
+        self.actionScheduleRemove = QAction(MainWindow)
+        self.actionScheduleRemove.setObjectName(u"actionScheduleRemove")
+
+        self.actionScheduleEdit = QAction(MainWindow)
+        self.actionScheduleEdit.setObjectName(u"actionScheduleEdit")
+
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayoutWidget = QWidget(self.centralwidget)
@@ -101,19 +113,28 @@ class Ui_MainWindow(object):
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
 
-        self.label = QLabel(self.gridLayoutWidget)
-        self.label.setObjectName(u"label")
+        self.LabelServerIP = QLabel(self.gridLayoutWidget)
+        self.LabelServerIP.setObjectName(u"LabelServerIP")
+
+        self.LabelLatency = QLabel(self.gridLayoutWidget)
+        self.LabelLatency.setObjectName(u"LabelLatency")
+
+        self.LabelPing = QLabel(self.gridLayoutWidget)
+        self.LabelPing.setObjectName(u"LabelPing")
 
         self.Label_online = QLabel("Online")
         self.Label_online.setAlignment(Qt.AlignRight)  # Align the label to the right
 
         # Create a QHBoxLayout for the labels
         self.labelLayout = QHBoxLayout()
-        self.labelLayout.addWidget(self.label)  # Add the server IP address label
-        self.labelLayout.addWidget(self.Label_online)  # Add the "Online:" label
+        self.labelLayout.addWidget(self.LabelServerIP)
+        self.labelLayout.addWidget(self.LabelLatency)
+        self.labelLayout.addWidget(self.LabelPing)
+        self.labelLayout.addWidget(self.Label_online)
 
         # Create a QTextEdit for the file editor
         self.EditFile = QTextEdit(self.gridLayoutWidget)
+        self.EditFile.setStyleSheet("background-color: #6ecdeb;")
         self.EditFile.setObjectName(u"EditFile")
         self.EditFile.hide()  # Hide the editor by default
 
@@ -123,6 +144,7 @@ class Ui_MainWindow(object):
 
         # Create a QPushButton for the save button
         self.SaveButton = QPushButton("Save", self.gridLayoutWidget)
+        self.SaveButton.setStyleSheet("background-color: #5daac2;")
         self.SaveButton.setObjectName(u"SaveButton")
         self.SaveButton.clicked.connect(self.save_file)
         self.SaveButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
@@ -130,6 +152,7 @@ class Ui_MainWindow(object):
 
         # Create a QPushButton for the close button
         self.CloseButton = QPushButton("Close", self.gridLayoutWidget)
+        self.CloseButton.setStyleSheet("background-color: #5daac2;")
         self.CloseButton.setObjectName(u"CloseButton")
         self.CloseButton.clicked.connect(self.close_editor)
         self.CloseButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
@@ -159,8 +182,10 @@ class Ui_MainWindow(object):
 
         self.Button_Send_Command = QPushButton(self.gridLayoutWidget)
         self.Button_Send_Command.setObjectName(u"Button_Send_Command")
+        self.Button_Send_Command.setStyleSheet("background-color: #5daac2;")
 
         self.Line_Server_Console_Input = QLineEdit(self.gridLayoutWidget)
+        self.Line_Server_Console_Input.setStyleSheet("background-color: #6ecdeb;")
         self.Line_Server_Console_Input.setObjectName(u"Line_Server_Console_Input")
         # Set the size policy of Line_Server_Console_Input to Expanding
         self.Line_Server_Console_Input.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
@@ -170,6 +195,7 @@ class Ui_MainWindow(object):
         self.inputLayout.addWidget(self.Button_Send_Command)
 
         self.Server_Console = QTextEdit(self.gridLayoutWidget)
+        self.Server_Console.setStyleSheet("background-color: #6ecdeb;")
         self.Server_Console.setObjectName(u"Server_Console")
         # Add the Server_Console to the grid layout with a column span of 2
         self.gridLayout.addWidget(self.Server_Console, 2, 0, 1, 2)  # Span 2 columns
@@ -183,16 +209,20 @@ class Ui_MainWindow(object):
 
         # Then add the buttons
         self.Button_Start_Server = QPushButton("Start Server")
+        self.Button_Start_Server.setStyleSheet("background-color: #5daac2;")
         self.buttonLayout.addWidget(self.Button_Start_Server)
         self.Button_Start_Server.clicked.connect(self.start_server)
 
         self.Button_Restart_Server = QPushButton("Restart Server")
+        self.Button_Restart_Server.setStyleSheet("background-color: #5daac2;")
         self.buttonLayout.addWidget(self.Button_Restart_Server)
 
         self.Button_StopServer = QPushButton("Stop Server")
+        self.Button_StopServer.setStyleSheet("background-color: #5daac2;")
         self.buttonLayout.addWidget(self.Button_StopServer)
 
         self.Button_Kill_Server = QPushButton("Kill Server")
+        self.Button_Kill_Server.setStyleSheet("background-color: #5daac2;")
         self.buttonLayout.addWidget(self.Button_Kill_Server)
 
         # Add the button layout to the grid layout
@@ -212,31 +242,56 @@ class Ui_MainWindow(object):
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 1126, 22))
+
         self.menuGeneral = QMenu(self.menubar)
         self.menuGeneral.setObjectName(u"menuGeneral")
+        self.menuGeneral.setStyleSheet("background-color: #41cefa;")
+
         self.menuProperties = QMenu(self.menubar)
         self.menuProperties.setObjectName(u"menuProperties")
+        self.menuProperties.setStyleSheet("background-color: #41cefa;")
+
         self.menuPlugins = QMenu(self.menubar)
         self.menuPlugins.setObjectName(u"menuPlugins")
+        self.menuPlugins.setStyleSheet("background-color: #41cefa;")
+
+        self.menuSchedule = QMenu(self.menubar)
+        self.menuSchedule.setObjectName(u"menuSchedule")
+        self.menuSchedule.setStyleSheet("background-color: #41cefa;")
+
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
+        self.statusbar.setStyleSheet("background-color: #41cefa;")
         MainWindow.setStatusBar(self.statusbar)
 
         self.menubar.addAction(self.menuGeneral.menuAction())
         self.menubar.addAction(self.menuProperties.menuAction())
         self.menubar.addAction(self.menuPlugins.menuAction())
+        self.menubar.addAction(self.menuSchedule.menuAction())
+
         self.menuGeneral.addAction(self.actionOpen_config)
+
         self.menuGeneral.addSeparator()
+
         self.menuGeneral.addAction(self.actionOpen_EULA)
         self.menuGeneral.addAction(self.actionSelect_Server_jar)
+
         self.menuProperties.addAction(self.actionserver_properties)
         self.menuProperties.addAction(self.actionops_json)
+
         self.menuProperties.addSeparator()
+
         self.menuProperties.addAction(self.actionbanned_players_json)
         self.menuProperties.addAction(self.actionbanned_ips_json)
+
         self.menuPlugins.addAction(self.actionManage)
+        self.menuPlugins.addSeparator()
         self.menuPlugins.addAction(self.actionDownload)
+
+        self.menuSchedule.addAction(self.actionScheduleAdd)
+        self.menuSchedule.addAction(self.actionScheduleRemove)
+        self.menuSchedule.addAction(self.actionScheduleEdit)
 
         self.retranslateUi(MainWindow)
 
@@ -245,20 +300,27 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Minecraft Server Manager", None))
         self.actionOpen_EULA.setText(QCoreApplication.translate("MainWindow", u"Open EULA", None))
         self.actionSelect_Server_jar.setText(QCoreApplication.translate("MainWindow", u"Select Server JAR", None))
         self.actionManage.setText(QCoreApplication.translate("MainWindow", u"Manage", None))
         self.actionDownload.setText(QCoreApplication.translate("MainWindow", u"Download", None))
-        self.actionserver_properties.setText(QCoreApplication.translate("MainWindow", u"Server Setings", None))
+
+        self.actionScheduleAdd.setText(QCoreApplication.translate("MainWindow", u"Add Schedule", None))
+        self.actionScheduleRemove.setText(QCoreApplication.translate("MainWindow", u"Remove Schedule", None))
+        self.actionScheduleEdit.setText(QCoreApplication.translate("MainWindow", u"Edit Schedule", None))
+
+        self.actionserver_properties.setText(QCoreApplication.translate("MainWindow", u"Server Properties", None))
         self.actionops_json.setText(QCoreApplication.translate("MainWindow", u"Operators", None))
         self.actionbanned_players_json.setText(QCoreApplication.translate("MainWindow", u"Banned Players", None))
         self.actionbanned_ips_json.setText(QCoreApplication.translate("MainWindow", u"Banned IPs", None))
         self.actioncommands_yml.setText(QCoreApplication.translate("MainWindow", u"commands.yml", None))
-        self.actionOpen_config.setText(QCoreApplication.translate("MainWindow", u"Open manager config", None))
+        self.actionOpen_config.setText(QCoreApplication.translate("MainWindow", u"Open manager settings", None))
         self.Label_online.setText(QCoreApplication.translate("MainWindow", u"Online:", None))
         self.Button_Send_Command.setText(QCoreApplication.translate("MainWindow", u"Send Command", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Serves IP Adress: 127.0.0.1:25565", None))
+        self.LabelServerIP.setText(QCoreApplication.translate("MainWindow", u"Serves IP Adress: 127.0.0.1:25565", None))
+        self.LabelLatency.setText(QCoreApplication.translate("MainWindow", u"Serves Latency:", None))
+        self.LabelPing.setText(QCoreApplication.translate("MainWindow", u"Serves Ping:", None))
         self.Button_Start_Server.setText(QCoreApplication.translate("MainWindow", u"Start Server", None))
         self.Button_Restart_Server.setText(QCoreApplication.translate("MainWindow", u"Restart Server", None))
         self.Button_StopServer.setText(QCoreApplication.translate("MainWindow", u"Stop Server", None))
@@ -266,6 +328,7 @@ class Ui_MainWindow(object):
         self.menuGeneral.setTitle(QCoreApplication.translate("MainWindow", u"General", None))
         self.menuProperties.setTitle(QCoreApplication.translate("MainWindow", u"Properties", None))
         self.menuPlugins.setTitle(QCoreApplication.translate("MainWindow", u"Plugins", None))
+        self.menuSchedule.setTitle(QCoreApplication.translate("MainWindow", u"Schedule", None))
 
     # retranslateUi
 
@@ -311,10 +374,6 @@ class Ui_MainWindow(object):
             manager_settings = json.load(json_file)
 
         server_dir = manager_settings["server_dir"]
-        print(f"Absolute path to server.jar: {os.path.abspath(os.path.join(server_dir, 'server.jar'))}")
-
-        print(
-            f"Attempting to access server JAR at: {os.path.abspath(os.path.join(server_dir, 'server.jar'))}")  # Print the absolute path
 
         # Check if "server.jar" exists in the specified directory
         if not os.path.exists(os.path.join(server_dir, 'server.jar')):
@@ -361,9 +420,19 @@ class Ui_MainWindow(object):
         server_dir = manager_settings["server_dir"]
         eula_path = os.path.join(server_dir, 'eula.txt')
 
-        # Open the EULA file and set the contents to the EditFile widget
-        with open(eula_path, "r") as file:
-            self.EditFile.setText(file.read())
+        try:
+            # Open the EULA file and set the contents to the EditFile widget
+            with open(eula_path, "r") as file:
+                self.EditFile.setText(file.read())
+        except FileNotFoundError:
+            # Check if "server.jar" exists in the specified directory
+            if not os.path.exists(os.path.join(server_dir, 'server.jar')):
+                error_message = (f"ERROR: file eula.txt not found: Try selecting server JAR first and run the server "
+                                 f"once!")
+
+                # Add the error message to the Server_Console without line number
+                self.Server_Console.append(error_message)
+                return
 
         # Show the EditFile widget, the SaveButton, and the CloseButton
         self.EditFile.show()
@@ -381,6 +450,7 @@ class Ui_MainWindow(object):
         # Save the contents of the EditFile widget to the EULA file
         with open(eula_path, "w") as file:
             file.write(self.EditFile.toPlainText())
+            self.Server_Console.append("SUCCESS: eula.txt is saved!")
 
         # Show the CloseButton
         self.CloseButton.show()
